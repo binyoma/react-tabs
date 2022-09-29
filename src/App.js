0761ed634af892857816ed930550b2c87b717d2a
binyoma <innocentbinyoma@pm.me>
2022-09-29 17:02:24 +0200

    btn nav

--- src/App.js
+++ src/App.js
@@ -24,33 +24,42 @@ function App() {
       </section>
     );
   } else {
-  
-    const {company, dates, duties,title}=jobs[value];
-    return <section className="section">
-      <div className="title">
-      <h2>Experience</h2>
-      <div className="underline"></div>
-      </div>
-      <div className="jobs-center">
-        {/* btn container*/}
-         {/* job info */}
-         <article className="job-info">
-          <h3>{title}</h3>
-          <h4>{company}</h4>
-          <p className="job-date">{dates}</p>
-          {duties.map((duty,index)=>{
-            return <div key={index} className="job-desc">
-              <FaAngleDoubleRight className="job-icon"/>
-              <p>{duty}</p>
-
-            </div>
-          })}
-
-         </article>
-
-      </div>
-    
-    </section>;
+    const { company, dates, duties, title } = jobs[value];
+    return (
+      <section className="section">
+        <div className="title">
+          <h2>Experience</h2>
+          <div className="underline"></div>
+        </div>
+        <div className="jobs-center">
+          {/* btn container*/}
+          <div className="btn-container">
+            {jobs.map((item, index) => {
+              return (
+                <button key={item.id} onClick={() => setValue(index)} 
+                className={`job-btn ${index=== value && 'active-btn'}`}>
+                  {item.company}
+                </button>
+              );
+            })}
+          </div>
+          {/* job info */}
+          <article className="job-info">
+            <h3>{title}</h3>
+            <h4>{company}</h4>
+            <p className="job-date">{dates}</p>
+            {duties.map((duty, index) => {
+              return (
+                <div key={index} className="job-desc">
+                  <FaAngleDoubleRight className="job-icon" />
+                  <p>{duty}</p>
+                </div>
+              );
+            })}
+          </article>
+        </div>
+      </section>
+    );
   }
 }
 
